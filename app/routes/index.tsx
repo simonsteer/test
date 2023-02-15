70da0702029c1ef007e7b45e8660ad0f27737e38
Simon Steer <simonpsteer@gmail.com>
2023-02-15 11:55:55 -0500

    hello world

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,8 @@
 export default function Index() {
+  console.log('hello world')
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
       <h1>Welcome to Remix</h1>
       <ul>
         <li>
@@ -28,5 +30,5 @@ export default function Index() {
         </li>
       </ul>
     </div>
-  );
+  )
 }
